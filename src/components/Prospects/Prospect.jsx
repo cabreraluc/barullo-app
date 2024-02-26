33eadaa2d44821a56a4569af6fb05982abcd4f00
cabreraluc <cabreralucaspatricio@gmail.com>
2024-02-26 16:57:52 -0300

    clientRol

--- src/components/Prospects/Prospect.jsx
+++ src/components/Prospects/Prospect.jsx
@@ -8,7 +8,7 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import EditIcon from "@mui/icons-material/Edit";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useRouteLoaderData } from "react-router-dom";
 import AlertDialog from "../Dialog/AlertDialog";
 import { useState, useEffect } from "react";
 import useProspects from "../../containers/Prospects/useProspects";
@@ -19,6 +19,9 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import { RedesContainer } from "../../containers/Prospects/prospectsStyles";
+import useAuth from "../../containers/Login/useAuth";
+import useUsers from "../../containers/Users/useUsers";
+import Loader from "../../componentsCss/Loader/Loader";
 
 export default function Prospect({
   prospect,
@@ -34,6 +37,14 @@ export default function Prospect({
   const [interestLevelState, setInterestLevelState] = useState({
     interestLevel: "-",
   });
+
+  const { getUserById, user } = useUsers();
+
+  const userLocalStorage = useAuth();
+
+  useEffect(() => {
+    getUserById(userLocalStorage.id);
+  }, []);
 
   const handlerDeleteProspect = (id) => {
     disableProspect(id);
@@ -71,8 +82,12 @@ export default function Prospect({
       >
         {prospect.name} {prospect.lastName}
       </TableCell>
-      <TableCell align="left">{prospect.cellphone}</TableCell>
-      <TableCell align="left">{prospect.email}</TableCell>
+      <TableCell align="left">
+        {prospect?.cellphone?.length ? prospect.cellphone : "---"}
+      </TableCell>
+      <TableCell align="left">
+        {prospect?.email?.length ? prospect.email : "---"}
+      </TableCell>
       <TableCell align="left">
         {" "}
         <FormControl>
@@ -80,6 +95,7 @@ export default function Prospect({
             MenuProps={{ disableScrollLock: true }}
             onChange={handleChangeInterestLevel}
             value={interestLevelState.interestLevel}
+            disabled={user.role === "Client" ? true : false}
             sx={{
               height: "2rem",
               width: "9rem",
@@ -97,6 +113,7 @@ export default function Prospect({
             MenuProps={{ disableScrollLock: true }}
             onChange={handleChange}
             value={statusOfProspect.status}
+            disabled={user.role === "Client" ? true : false}
             sx={{
               height: "2rem",
               width: "9rem",
@@ -109,62 +126,85 @@ export default function Prospect({
         </FormControl>
       </TableCell>
       <TableCell>
-        <RedesContainer>
-          {prospect.instagram.length ? (
-            <a
-              style={{ color: "black" }}
-              href={prospect.instagram}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <InstagramIcon fontSize="small" />
-            </a>
-          ) : null}
-          {prospect.facebook.length ? (
-            <a
-              style={{ color: "black" }}
-              href={prospect.facebook}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FacebookIcon fontSize="small" />
-            </a>
-          ) : null}
-          {prospect.linkedin.length ? (
-            <a
-              style={{ color: "black" }}
-              href={prospect.linkedin}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <LinkedInIcon fontSize="small" />
-            </a>
-          ) : null}
-          {prospect.tiktok.length ? (
-            <a
-              style={{ color: "black" }}
-              href={prospect.tiktok}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FacebookIcon fontSize="small" />
-            </a>
-          ) : null}
-        </RedesContainer>
+        {!prospect?.instagram?.length &&
+        !prospect?.linkedin?.length &&
+        !prospect?.facebook?.length &&
+        !prospect?.tiktok?.length ? (
+          "---"
+        ) : (
+          <RedesContainer>
+            {prospect.instagram.length ? (
+              <a
+                style={{ color: "black" }}
+                href={prospect.instagram}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <InstagramIcon fontSize="small" />
+              </a>
+            ) : null}
+            {prospect.facebook.length ? (
+              <a
+                style={{ color: "black" }}
+                href={prospect.facebook}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FacebookIcon fontSize="small" />
+              </a>
+            ) : null}
+            {prospect.linkedin.length ? (
+              <a
+                style={{ color: "black" }}
+                href={prospect.linkedin}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <LinkedInIcon fontSize="small" />
+              </a>
+            ) : null}
+            {prospect.tiktok.length ? (
+              <a
+                style={{ color: "black" }}
+                href={prospect.tiktok}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FacebookIcon fontSize="small" />
+              </a>
+            ) : null}
+          </RedesContainer>
+        )}
       </TableCell>
 
       <TableCell align="left">
-        <DeleteOutlineIcon
-          onClick={() => setOpen(true)}
-          sx={{ cursor: "pointer" }}
-        />
+        {prospect?.country?.length ? prospect.country : "---"}
       </TableCell>
-      <TableCell align="left">
-        <EditIcon
-          onClick={() => navigate(`/edit-prospect/${prospect._id}`)}
-          sx={{ cursor: "pointer" }}
-        />
-      </TableCell>
+      {user.role === "Client" ? null : user.role?.length ? (
+        <TableCell align="left">
+          <DeleteOutlineIcon
+            onClick={() => setOpen(true)}
+            sx={{ cursor: "pointer" }}
+          />
+        </TableCell>
+      ) : (
+        <TableCell align="left">
+          <Loader size={15}></Loader>
+        </TableCell>
+      )}
+
+      {user.role === "Client" ? null : user.role?.length ? (
+        <TableCell align="left">
+          <EditIcon
+            onClick={() => navigate(`/edit-prospect/${prospect._id}`)}
+            sx={{ cursor: "pointer" }}
+          />
+        </TableCell>
+      ) : (
+        <TableCell align="left">
+          <Loader size={15}></Loader>
+        </TableCell>
+      )}
       <AlertDialog
         open={open}
         onClose={() => setOpen(false)}
