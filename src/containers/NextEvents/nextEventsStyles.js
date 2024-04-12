9e03702f3a2fed4cb153572305c1bbb99763fad3
cabreraluc <cabreralucaspatricio@gmail.com>
2024-04-12 11:46:10 -0300

    changes

--- src/containers/NextEvents/nextEventsStyles.js
+++ src/containers/NextEvents/nextEventsStyles.js
@@ -27,7 +27,7 @@ export const Container = styled.div`
   justify-content: center;
   align-items: end;
   background-color: rgb(0, 0, 0, 0.5);
-  height: 20vh;
+  height: 20%;
   width: 100%;
   flex-direction: row;
   padding: 2rem;
