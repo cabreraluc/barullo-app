bff4087e6f5b967e368b244d9f6a418905ea2c3e
cabreraluc <cabreralucaspatricio@gmail.com>
2024-04-12 12:03:59 -0300

    changes

--- src/containers/NextEvents/nextEventsStyles.js
+++ src/containers/NextEvents/nextEventsStyles.js
@@ -4,7 +4,7 @@ export const NextEventsContainer = styled.div`
   display: flex;
   justify-content: center;
 
-  height: 100vh;
+  height: 100%;
 `;
 export const Card = styled.div`
   background: ${({ image }) =>
@@ -13,7 +13,7 @@ export const Card = styled.div`
   justify-content: flex-end;
   align-items: center;
   width: 100vw;
-  height: 100vh;
+  height: 100%;
   flex-direction: column;
   color: black;
 
