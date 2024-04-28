68290c867391776fc48cf1058eb0f74f4834be00
cabreraluc <cabreralucaspatricio@gmail.com>
2024-04-28 19:06:36 -0300

    estilos

--- src/containers/Merch/merchStyles.js
+++ src/containers/Merch/merchStyles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const MerchContainer = styled.div`
   display: flex;
   justify-content: center;
-  height: 100%;
+  height: 100vh;
 `;
 // margin-top: calc(15vh + 6vw);
 export const Card = styled.div`
