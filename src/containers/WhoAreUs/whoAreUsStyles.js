a98adcb9c231469af03014c0453511867faec95e
cabreraluc <cabreralucaspatricio@gmail.com>
2024-04-28 18:28:31 -0300

    styles

--- src/containers/WhoAreUs/whoAreUsStyles.js
+++ src/containers/WhoAreUs/whoAreUsStyles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const WhoAreUsContainer = styled.div`
   display: flex;
   justify-content: center;
-  height: 100vh;
+  height: 100%;
 `;
 
 export const Card = styled.div`
