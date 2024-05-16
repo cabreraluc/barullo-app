8d50d8854bacd325603695260486c3bbae780196
Lucas Patricio Cabrera <102101114+cabreraluc@users.noreply.github.com>
2024-05-16 00:50:58 -0300

    Update constants.jsx

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -4,7 +4,7 @@ export const BotonesPaymentSection = [
   { text: "x1 Tickets $4500", promo: 1 },
   { text: "x2 Tickets $8000", promo: 2 },
   { text: "x5 Tickets $18000", promo: 3 },
-  { text: "x10 Tickets $30000", promo: 4 },
+ 
 ];
 
 export const InputsDePaymentSection = [
