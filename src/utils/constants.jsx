14e589f37b58c145394979e80b1ced8563fb77b7
Lucas Patricio Cabrera <102101114+cabreraluc@users.noreply.github.com>
2024-05-20 22:38:24 -0300

    Update constants.jsx

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,9 +1,9 @@
 export const sectionsAdminPanel = ["eventos", "artistas", "usuarios"];
 
 export const BotonesPaymentSection = [
-  { text: "x1 Tickets $5500", promo: 1 },
-  { text: "x2 Tickets $9000", promo: 2 },
-  { text: "x5 Tickets $2000", promo: 3 },
+  { text: "x1 Tickets $5.500", promo: 1 },
+  { text: "x2 Tickets $9.000", promo: 2 },
+  { text: "x5 Tickets $20.000", promo: 3 },
  
 ];
 
